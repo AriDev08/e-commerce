98dd0e84f07941e637cb43f19bedd220aaf65b7e
NyssINI <igustingurahdenysyudiantara@gmail.com>
2025-06-25 21:14:04 +0800

    interaksi perloginan

--- landingpage/index.php
+++ landingpage/index.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-</head>
-
 <body>
-    <section class=" w-full h-screen" style="background-position: center bottom;  background-image: url('hero.png');">
+    <section class="w-full h-screen" style="background-position: center bottom; background-image: url('hero.png');">
         <nav class="container mx-auto">
             <ul class="flex justify-between items-center mt-3 m-8">
                 <li>
                     <ul class="flex items-center gap-6">
                         <li class="text-[36px] flex items-center gap-2">
-                            Shop <img class="w-[34px] h-[34px] mt-2" src="arrow.png" alt="">
+                            Shop
+                            <img class="w-[34px] h-[34px] mt-2" src="arrow.png" alt="Arrow" />
                         </li>
                         <li class="text-[36px]">About</li>
                         <li class="text-[36px]">News</li>
@@ -24,149 +22,144 @@
                 </li>
                 <li>
                     <ul class="flex items-center gap-9">
-                        <li><img class="w-[41px] h-[41px]" src="shopping-chart.png" alt=""></li>
                         <li>
-                        <?php
-                             session_start(); 
-                             if (isset($_SESSION['username'])) {
-                            // Sudah login
-                            if ($_SESSION['role'] === 'admin') {
-                             $link = '../dashboard/index.php';
-                             } else {
-                            $link = 'landingpage/index.php'; 
-                             }
-                             } else {
-                             $link = 'login.php';
-                            }
-                        ?>
-                            <a href="<?= $link ?>">
-                                <button class="w-20 h-10 bg-[#757575] rounded-lg">LOGIN</button>
-                            </a>
+                            <img class="w-[41px] h-[41px]" src="shopping-chart.png" alt="Cart" />
                         </li>
-                        <li><img class="w-[36px] h-[36px]" src="search.png" alt=""></li>
+                        <li>
+                            <?php
+                            session_start();
+                            if (isset($_SESSION['username'])) {
+                                $link = ($_SESSION['role'] === 'admin') ? '../dashboard/index.php' : 'profile.php';
+                                echo '
+                                <a href="' . $link . '" class="flex items-center gap-2">
+                                    <img src="landingpage/user.png" alt="User" class="w-8 h-8 rounded-full">
+                                    <span class="text-white">' . htmlspecialchars($_SESSION['username']) . '</span>
+                                </a>';
+                            } else {
+                                echo '
+                                <a href="login.php">
+                                    <button class="w-20 h-10 bg-[#757575] text-white rounded-lg">LOGIN</button>
+                                </a>';
+                            }
+                            ?>
+                        </li>
+                        <li>
+                            <img class="w-[36px] h-[36px]" src="search.png" alt="Search" />
+                        </li>
                     </ul>
                 </li>
             </ul>
         </nav>
-
         <div class="flex justify-center items-center -ml-24">
-            <h1 class="text-[64px] flex justify-center items-center max-w-ms -ml-40 font-bold">Your one-stop Website <span class="mt-40 -ml-110">Maker.</span></h1>
+            <h1 class="text-[64px] flex justify-center items-center max-w-ms -ml-40 font-bold">
+                Your one-stop Website <span class="mt-40 -ml-110">Maker.</span>
+            </h1>
             <a href="checkhout.php">
-            <div class="bg-gray-900/30 rounded-lg w-[160px] h-[57px] flex justify-center items-center -ml-46 mt-80 hover:bg-gray-900/20 duration-700">
-                <h1 class="text-[32px]">Customize</h1>
-            </div>
+                <div class="bg-gray-900/30 rounded-lg w-[160px] h-[57px] flex justify-center items-center -ml-46 mt-80 hover:bg-gray-900/20 duration-700">
+                    <h1 class="text-[32px]">Customize</h1>
+                </div>
             </a>
         </div>
     </section>
-    <section class="w-full h-screen h-14 h-14 bg-linear-to-t from-[#FFFFFF] to-[#B1B1B1] p-5">
+    <section class="w-full h-screen bg-gradient-to-t from-[#FFFFFF] to-[#B1B1B1] p-5">
         <h1 class="text-[40px] ml-30 pt-3">SUIT YOUR NEEDS</h1>
         <div class="flex gap-20 justify-center items-center pt-4">
             <a href="">
                 <div class="bg-[#757575] w-[350px] h-[600px] text-center pt-70 text-[40px] hover:scale-105 duration-300 hover:bg-[#757575]/50">PORTOFOLIO</div>
-            </a> 
-             <a href="">
+            </a>
+            <a href="">
                 <div class="bg-[#757575] w-[350px] h-[600px] text-center pt-70 text-[40px] hover:scale-105 duration-300 hover:bg-[#757575]/50">COMPANY PROFILE</div>
-            </a> 
-             <a href="">
+            </a>
+            <a href="">
                 <div class="bg-[#757575] w-[350px] h-[600px] text-center pt-70 text-[40px] hover:scale-105 duration-300 hover:bg-[#757575]/50">COMMERCIAL</div>
-            </a> 
+            </a>
         </div>
     </section>
-    
     <section class="w-full h-screen pt-10">
         <h1 class="text-[40px] ml-10">Our Expertise,</h1>
-        <!-- ATAS -->
-        <div class="gap-20 flex mt-5 ml-2 text-[40px] flex justify-center items-center">
+        <div class="gap-20 flex mt-5 ml-2 text-[40px] justify-center items-center">
             <a href="">
-            <div class="bg-[#D4D4D4] w-[305px] h-[100px] rounded-xl p-5 pl-25   ">Next.js
-            </div>
+                <div class="bg-[#D4D4D4] w-[305px] h-[100px] rounded-xl p-5 pl-25">Next.js</div>
             </a>
             <a href="">
-            <div class="bg-[#D4D4D4] w-[305px] h-[110px] rounded-xl p-5 pl-25">Next.js
-            </div>
+                <div class="bg-[#D4D4D4] w-[305px] h-[110px] rounded-xl p-5 pl-25">Next.js</div>
             </a>
             <a href="">
-            <div class="bg-[#D4D4D4] w-[305px] h-[100px] rounded-xl p-5 pl-25">Next.js
-            </div>
+                <div class="bg-[#D4D4D4] w-[305px] h-[100px] rounded-xl p-5 pl-25">Next.js</div>
             </a>
             <a href="">
-            <div class="bg-[#D4D4D4] w-[305px] h-[100px] rounded-xl p-5 pl-25">Next.js
-            </div>
+                <div class="bg-[#D4D4D4] w-[305px] h-[100px] rounded-xl p-5 pl-25">Next.js</div>
             </a>
         </div>
         <h1 class="text-[40px] ml-10 mt-5">Your <span class="font-bold">Choice</span></h1>
         <div class="flex gap-20 ml-10">
             <a href="shopping.php">
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
-            <a href="shopping.php">    
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
-            </a>
-            <a href="">
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
+            <a href="shopping.php">
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
             <a href="">
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
         </div>
-
-        <!-- BAWAH -->
         <div class="flex gap-20 mt-10 ml-10">
             <a href="">
-        <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
             <a href="">
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
             <a href="">
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                 <p>RP.xxx,xxx</p>
-            </div>
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
             <a href="">
-            <div class="w-[305px] h-[281px]">
-                <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
-                <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
-                <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
-                <p>RP.xxx,xxx</p>
-            </div>
+                <div class="w-[305px] h-[281px]">
+                    <div class="bg-[#D4D4D4] w-[305px] h-[180px] rounded-xl"></div>
+                    <h1 class="text-black text-[24px] font-bold">Nama Template</h1>
+                    <p class="text-black text-[20px]">deskripsi template deskripsi templatedeskripsi template</p>
+                    <p>RP.xxx,xxx</p>
+                </div>
             </a>
-        </div>
         </div>
     </section>
 </body>
-
 </html>
--- landingpage/profile.php
+++ landingpage/profile.php
@@ -1,70 +1,62 @@
+<?php
+session_start();
+// nanti koneksinya cocokin
+// include 'koneksi.php'; 
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+    exit;
+}
+$username = $_SESSION['username'];
+$query = "SELECT * FROM users WHERE username = '$username'";
+$result = mysqli_query($conn, $query);
+$data = mysqli_fetch_assoc($result);
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Profil Pengguna</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
-<body class="overflow-y-hidden">
-    <nav class="container mx-auto px-8 mt-3">
-            <ul class="flex justify-between items-center">
-                <li>
-                    <ul class="flex items-center gap-6">
-                        <li class="text-[36px] flex items-center gap-2">
-                            Shop <img class="w-[34px] h-[34px] mt-2" src="arrow.png" alt="">
-                        </li>
-                        <li class="text-[36px]">About</li>
-                        <li class="text-[36px]">News</li>
-                    </ul>
+<body class="overflow-y-hidden bg-gray-100">
+<nav class="container mx-auto px-8 mt-3">
+    <ul class="flex justify-between items-center">
+        <li>
+            <ul class="flex items-center gap-6">
+                <li class="text-[36px] flex items-center gap-2">
+                    Shop <img class="w-[34px] h-[34px] mt-2" src="arrow.png" alt="">
                 </li>
-
-                <li>
-                    <ul class="flex items-center gap-9">
-                        <li><img class="w-[41px] h-[41px]" src="shopping-chart.png" alt=""></li>
-                        <li><img class="w-[41px] h-[41px]" src="user.png" alt=""></li>
-                        <li><img class="w-[36px] h-[36px]" src="search.png" alt=""></li>
-                    </ul>
-                </li>
+                <li class="text-[36px]">About</li>
+                <li class="text-[36px]">News</li>
             </ul>
-        </nav>
-    <section class=" w-full h-screen">
-        <section class="w-full h-screen flex justify-center">
-            <div class="flex flex-col items-center">
-                <svg class="w-[213px] h-[213px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                    fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                        clip-rule="evenodd" />
-                </svg>
-                <h1 class="text-[64px] font-semibold">WELCOME</h1>
-                <div class="bg-[#D9D9D9] w-[1230px] h-[450px] p-20 pl-50 rounded-lg mx-auto mb-30">
-                    <div class="flex text-[40px]">
-                        <div class="flex-1 space-y-4">
-                            <p><span class="font-bold">Biodata:</span> Biodata</p>
-                            <p><span class="font-bold">Biodata:</span> Biodata</p>
-                            <p><span class="font-bold">Biodata:</span> Biodata</p>
-                        </div>
-
-                        <hr class="h-60 border-l-2 border-black" />
-
-                        <div class="flex-1 space-y-4 pl-30">
-                            <p><span class="font-bold">Biodata:</span> Biodata</p>
-                            <p><span class="font-bold">Biodata:</span> Biodata</p>
-                            <p><span class="font-bold">Biodata:</span> Biodata</p>
-                        </div>
-                    </div>
-                    <a href="">
-                            <div class="bg-gray-500 w-60 h-18 rounded-lg ml-74 p-5 pl-12 mt-3">
-                                <h1 class="font-semibold text-xl">Change Password</h1>
-                            </div>
-                         </a>
-                </div>
-
-        </section>
-
+        </li>
+        <li>
+            <ul class="flex items-center gap-9">
+                <li><img class="w-[41px] h-[41px]" src="shopping-chart.png" alt=""></li>
+                <li><img class="w-[41px] h-[41px]" src="user.png" alt=""></li>
+                <li><img class="w-[36px] h-[36px]" src="search.png" alt=""></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+<section class="w-full h-screen flex justify-center items-center">
+    <div class="bg-white shadow-lg rounded-lg w-[80%] p-10">
+        <h1 class="text-[48px] font-semibold text-center mb-10">WELCOME, <?= htmlspecialchars($data['username']) ?></h1>
+        
+        <div class="flex text-[24px] justify-between">
+            <div class="flex-1 space-y-4">
+                <p><span class="font-bold">Username:</span> <?= htmlspecialchars($data['username']) ?></p>
+                <p><span class="font-bold">Password:</span> <?= htmlspecialchars($data['password']) ?></p>
+                <p><span class="font-bold">Nomor Telepon:</span> <?= htmlspecialchars($data['no_tlp']) ?></p>
+            </div>
+        </div>
+        <a href="ubah_password.php">
+            <div class="bg-gray-500 w-60 h-18 rounded-lg mx-auto mt-10 text-center py-3">
+                <h1 class="font-semibold text-xl text-white">Change Password</h1>
+            </div>
+        </a>
+    </div>
+</section>
 </body>
-
 </html>
